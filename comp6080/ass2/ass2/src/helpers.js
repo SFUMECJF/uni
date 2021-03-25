1a12f2ff5d67535b1daf4343672f5db80eac0bc8
John Dao <work@johndao.dev>
2021-03-26 06:19:21 +1100

    removed uneccesary functions

--- comp6080/ass2/ass2/src/helpers.js
+++ comp6080/ass2/ass2/src/helpers.js
@@ -77,6 +77,5 @@ export function showModal(title, content) {
 
 // plucked from somwhere in the web
 export function checkEmail(email) { 
-    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);
+    return true;
 } 
